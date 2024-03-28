be914221f57db1ab3092250e7dd3bae06dcbe840
JMonalisa-98 <you@example.commonalisarout1999@gmail.com>
2024-03-28 13:35:00 +0530

    Monalisas changes

--- Performance/src/main/java/com/orive/Performance/Config/PerformanceConfig.java
+++ Performance/src/main/java/com/orive/Performance/Config/PerformanceConfig.java
@@ -3,14 +3,26 @@ package com.orive.Performance.Config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class PerformanceConfig {
+@EnableWebMvc
+public class PerformanceConfig implements WebMvcConfigurer{
 	
-	@Bean	
-	public ModelMapper modelMapper()
-	{
+	@Bean
+	public ModelMapper modelMapper(){
 		return new ModelMapper();
 	}
+	
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply CORS configuration to all paths
+                .allowedOrigins("*") // Allow requests from all origins
+                .allowedMethods("*") // Allow all HTTP methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true); // Allow credentials such as cookies, authorization headers
+    }
 
 }
